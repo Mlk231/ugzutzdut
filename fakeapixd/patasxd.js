e2e2b7932a9ddb1d14df2d026de2af343bf71d94
Mlk231 <91147304+Mlk231@users.noreply.github.com>
2021-09-22 11:39:04 -0500

    Update patasxd.js

--- fakeapixd/patasxd.js
+++ fakeapixd/patasxd.js
@@ -1,4 +1,3 @@
 [
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZEY4qQmwAJhKVFlypt0nZNgzrkApOax6vew&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfyqYAyrxqYswPhrZIfJz_wvS_56-zpPoWqg&usqp=CAU"}
+{"result": "https://pbs.twimg.com/media/Efld54SWsAEYeeh.jpg"},
 ]
